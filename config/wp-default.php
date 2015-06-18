aa790f93d0585fba32064f15936c14c3f323a79d
Jorge Cortes <jorge.cortes@gmail.com>
2015-06-18 12:02:43 -0700

    Remove the phrases and put the defaults

--- config/wp-default.php
+++ config/wp-default.php
@@ -14,14 +14,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'ek$-plyq;J<6ErNJ.SQzcH@y=ze+Rj9PW%8dg.ubU#{P=4rgP,~-|WQ8uZ(YhDH+');
-define('SECURE_AUTH_KEY',  'f#l^{H#}ye1+{b+XfBCZZe9g,w=Q~aoeG@-8}s`rXJY(bu:(,D++)p:y$+poFTNc');
-define('LOGGED_IN_KEY',    '*-=1P|2;V(Rxy+ebFYiP-u/)]H8%C,/x<jh#l-xdm/+NTdyf!S@qfv^kJk `LL{L');
-define('NONCE_KEY',        '8f}oJTC_4UM1P)Q(HFL )|Aw>i^t2ULi?}|27*`|I3VEaFK}8e-A2qCP8|<h>v&*');
-define('AUTH_SALT',        '-E5uiJ.%/.)H+qhVXJ*x6Fq3D7,1(L>SJKJdr-a=#mL:i`<c_@^?ALl<M1QEll%1');
-define('SECURE_AUTH_SALT', 'Y pu36Xv!YL]:I&U4`Nv5<cK!^WMa6/Pt`F-a[9v4=7.-i]oe23oT Xt}PLvK!8g');
-define('LOGGED_IN_SALT',   'Y_LcjlZ-Xp%-)M|mKB+k3FK_fl(B*)[HcQuDEV|!|tw@XrYj>!U3xyzQPn6AQ}G@');
-define('NONCE_SALT',       ':?`gByN YfTBYRg{Pk^8Yy4Nn%oB%<rX-Z)U=V=x*[}f)HVd81(}M%mOE6|Xo?/L');
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
 
 /**#@-*/
 
